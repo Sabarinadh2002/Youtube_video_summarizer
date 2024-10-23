e512a3db47a837ed41a6fdfd726ddb9d92d3817d
Sabarinadh2002 <140910234+Sabarinadh2002@users.noreply.github.com>
2024-10-23 14:23:38 +0100

    Your descriptive commit message here

--- app/data/index.ts
+++ app/data/index.ts
@@ -3,13 +3,10 @@ import git from '/public/git.svg';
 import link from '/public/link.svg';
 
 export const socialMedia = [
-    {
-      id: 1,
-      img: git,
-    },
+    
     
     {
-      id: 2,
+      id: 1,
       img: link,
     },
 ];
