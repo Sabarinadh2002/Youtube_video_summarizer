92b759e0e268bfea9b14967421acac4fd2e0ddfd
Sabarinadh2002 <140910234+Sabarinadh2002@users.noreply.github.com>
2024-11-06 12:13:32 +0000

    remove error for deploying website using vercel

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import { socialMedia } from '@/app/data'
 
 import React from 'react'
-import Image from 'next/image';
+
 
 
 const Footer = () => {
@@ -17,7 +17,7 @@ const Footer = () => {
             key={info.id}
             className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
           >
-             <Image src={info.img} alt="icons" width={20} height={20} />
+             
 
         
         </div>
